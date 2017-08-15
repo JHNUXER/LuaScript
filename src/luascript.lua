6b575d457f9c1b80be7cb8afb8a5640f2d3bfa9a
John Kayne <johnhoganuke2@gmail.com>
2017-08-15 14:59:14 -0400

    Update luascript.lua

--- src/luascript.lua
+++ src/luascript.lua
@@ -1,33 +1,35 @@
-LuaScript = {}
+Token = {
+	newFromValue = function(v)
+		local tbl = {
+			type = type(v),
+			value = v
+		}
+		setmetatable(tbl,Token)
+		return tbl
+	end,
+	newFromIdent = function(i)
+		local typ
+		if string.match(i,"([.])&") then typ = "functionCall" else typ = "variable" end
+		local tbl = {
+			type = typ,
+			value = i
+		}
+		setmetatable(tbl,Token)
+	end,
+	ADD = {
+		type = "operator",
+		
+	}
+}
 
-LuaScript.parse = function parse(sInput)
-  local tTokens = {}
-  setmetatable(tTokens,{add = function(x) table.insert(tTokens,x) end})
-  local tok = ""
-  local state = 0
-  local table = {sInput:gsub("[.]")}
-  for i = 1,#table do
-    local chr = table[i]
-    tok = tok + chr
-    if state == 0 then
-      if tok == "\"" then
-        state = 1
-      elseif tok == "function" then
-        state = 2
-      elseif tok == "\'" then
-        state = 1
-      elseif tok == "if" then
-        state = 3
-      elseif tok == "{" then
-        state = 4
-      elseif tok == "?" then
-        state = 5
-      end
-    end
-  end
-end
-
-LuaScript.exec = function(pCode)
-end
-
-LuaScript.compile = function(sInput) end
+Expression = {
+	new = function(tTokens)
+		setmetatable(tTokens,Expression)
+		return tTokens
+	end,
+	__unm = function(x)
+		for i = 1,#x do
+			type(x)
+		end
+	end
+}
